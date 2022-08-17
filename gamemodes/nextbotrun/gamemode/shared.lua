d2354c8ea46b19125d8a9466a608d86241c81bd8
n1clud3 <kolyabarabot07@gmail.com>
2022-08-17 17:12:42 +0300

    remove spawning concommands

--- gamemodes/nextbotrun/gamemode/shared.lua
+++ gamemodes/nextbotrun/gamemode/shared.lua
@@ -4,4 +4,10 @@ GM.Author = "n1clud3"
 DeriveGamemode("sandbox")
 
 function GM:Initialize()
+    -- Remove concommands that allow players to spawn props, sweps etc.
+    concommand.Remove("gm_spawn")
+    concommand.Remove("gm_spawnsent")
+    concommand.Remove("gm_spawnswep")
+    concommand.Remove("gm_spawnvehicle")
+    concommand.Remove("gm_giveswep")
 end
